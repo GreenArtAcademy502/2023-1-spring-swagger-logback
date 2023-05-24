ba8c8e90b9cb05731c638adc33e060d529e14292
506호 <greendg1502@gmail.com>
2023-05-24 11:34:47 +0900

    파일 업로드 작업 중

---
+++ src/main/java/com/green/board7/fileupload/FileuploadController.java
@@ -0,0 +1,24 @@
+package com.green.board7.fileupload;
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/fileupload")
+public class FileuploadController {
+    private final Logger LOGGER;
+    private FileuploadService service;
+
+    @Autowired
+    public FileuploadController(FileuploadService service) {
+        LOGGER = LoggerFactory.getLogger(FileuploadController.class);
+        this.service = service;
+    }
+
+    @PostMapping(consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE })
+    public void fileupload(@RequestPart MultipartFile img) {
+        LOGGER.info("imgFileName: " + img.getOriginalFilename());
+    }
+}
---
+++ src/main/java/com/green/board7/fileupload/FileuploadService.java
@@ -0,0 +1,8 @@
+package com.green.board7.fileupload;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileuploadService {
+
+}
